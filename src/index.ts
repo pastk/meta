0c45b20401395b6a670f513aebdd38e13bb540e3
Roman Tsisyk <roman@tsisyk.com>
2022-10-02 13:31:41 +0300

    Add a log message when donates are enabled
    Needed to count the number of users who actually received
    the new config.
    
    ```
    "logs":[{"message":["Donates enabled"],"level":"log","timestamp":1664702669575}]
    ```
    
    Co-authored-by: Alexander Borsuk <170263+biodranik@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -133,6 +133,8 @@ export async function handleRequest(request: Request) {
       }
 
       if (donatesEnabled) {
+        // To count enabled donations.
+        console.log('Donates enabled');
         if (request.cf?.country == 'RU') {
           response.settings = {
             DonateUrl: DONATE_URL_RU,
