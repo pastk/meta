6ce9fc1bdf5b1d1c25e28a00a7f83a9f52def47e
Roman Tsisyk <roman@tsisyk.com>
2023-04-02 21:51:27 +0300

    Add 230329

--- src/servers.ts
+++ src/servers.ts
@@ -8,46 +8,46 @@ export const SERVER = {
     url: 'https://cdn-us1.organicmaps.app/',
     dataVersions: [
       210529, 210703, 210729, 210825, 211002, 211022, 211122, 220103, 220204, 220314, 220415, 220515, 220613, 220718,
-      220816, 220912, 221029, 221119, 221216, 230121, 230210, 230227,
+      220816, 220912, 221029, 221119, 221216, 230121, 230210, 230227, 230329,
     ],
   },
   uk1: {
     // Mythic Beasts VPS (London, UK) 200TB/mo.
     url: 'https://cdn-uk1.organicmaps.app/',
-    dataVersions: [230121, 230210, 230227],
+    dataVersions: [230210, 230227, 230329],
   },
   nl1: {
     // // Mythic Beasts VPS (Amsterdam, NL) 200TB/mo.
     url: 'https://cdn-nl1.organicmaps.app/',
-    dataVersions: [230121, 230210, 230227],
+    dataVersions: [230210, 230227, 230329],
   },
   planet: {
     // Hetzner BareMetal (Falkenstein, Germany) unmetered
     url: 'https://cdn.organicmaps.app/',
     dataVersions: [
       220103, 220204, 220314, 220415, 220515, 220613, 220718, 220816, 220912, 221029, 221119, 221216, 230121, 230210,
-      230227,
+      230227, 230329,
     ],
   },
   fi1: {
     // Hetzner Cloud (Helsinki, Finland), 20TB/mo
     url: 'https://cdn-fi1.organicmaps.app/',
-    dataVersions: [230210, 230227],
+    dataVersions: [230227, 230329],
   },
   de1: {
     // Hetzner Cloud (Falkenstein, Germany), 20TB/mo
     url: 'https://cdn-eu2.organicmaps.app/',
-    dataVersions: [230210, 230227],
+    dataVersions: [230227, 230329],
   },
   de2: {
     // Hetzner Cloud (Falkenstein, Germany), 20TB/mo
     url: 'https://cdn-de2.organicmaps.app/',
-    dataVersions: [230121, 230210, 230227],
+    dataVersions: [230210, 230227, 230329],
   },
   us2: {
     // Hetzner Cloud (Asburn, US East), 20TB/mo
     url: 'https://cdn-us2.organicmaps.app/',
-    dataVersions: [230210, 230227],
+    dataVersions: [230227, 230329],
   },
 };
 
