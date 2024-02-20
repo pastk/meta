28e9b826d1beb615a9a56c45f64478ef6c3fc310
Roman Tsisyk <roman@tsisyk.com>
2024-02-20 23:23:26 +0300

    Enable donates for 240220

--- src/servers.ts
+++ src/servers.ts
@@ -123,7 +123,7 @@ export async function getServersList(request: Request) {
   };
 
   // Disable donates for reviewers for all app versions AFTER this one.
-  const lastApprovedAndReleasedGoogleAppVersionCode = 240206;
+  const lastApprovedAndReleasedGoogleAppVersionCode = 240220;
   const lastApprovedAndReleasediOSAppVersionCode = 240206;
   let donatesEnabled = true;
   if (
