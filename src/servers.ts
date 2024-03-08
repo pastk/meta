accf6a5607ef96177d32adfe8662090f49dfb6ec
Roman Tsisyk <roman@tsisyk.com>
2024-03-08 22:42:27 +0300

    Enable donates for 240307 (AppStore)

--- src/servers.ts
+++ src/servers.ts
@@ -124,7 +124,7 @@ export async function getServersList(request: Request) {
 
   // Disable donates for reviewers for all app versions AFTER this one.
   const lastApprovedAndReleasedGoogleAppVersionCode = 240303;
-  const lastApprovedAndReleasediOSAppVersionCode = 240303;
+  const lastApprovedAndReleasediOSAppVersionCode = 240307;
   let donatesEnabled = true;
   if (
     appVersion.flavor === 'google' &&
