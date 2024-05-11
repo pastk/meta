6bfc5c45c4fba2993e8e59a4914b85ce86ee04cb
Alexander Borsuk <me@alex.bio>
2024-05-11 23:05:08 +0200

    Enable donates iOS 240510
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- src/servers.ts
+++ src/servers.ts
@@ -124,7 +124,7 @@ export async function getServersList(request: Request) {
 
   // Disable donates for reviewers for all app versions AFTER this one.
   const lastApprovedAndReleasedGoogleAppVersionCode = 240331;
-  const lastApprovedAndReleasediOSAppVersionCode = 240331;
+  const lastApprovedAndReleasediOSAppVersionCode = 240510;
   let donatesEnabled = true;
   if (
     appVersion.flavor === 'google' &&
