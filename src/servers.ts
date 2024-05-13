c2ae1236eb6999e9ee14e0acf055ad4323418d88
Roman Tsisyk <roman@tsisyk.com>
2024-05-13 19:43:56 +0300

    Enable donations for 240511 (Google)
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- src/servers.ts
+++ src/servers.ts
@@ -123,7 +123,7 @@ export async function getServersList(request: Request) {
   };
 
   // Disable donates for reviewers for all app versions AFTER this one.
-  const lastApprovedAndReleasedGoogleAppVersionCode = 240331;
+  const lastApprovedAndReleasedGoogleAppVersionCode = 240511;
   const lastApprovedAndReleasediOSAppVersionCode = 240510;
   let donatesEnabled = true;
   if (
