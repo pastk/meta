9fc90d1dd5a3c77897e6120e25c4b4411d070cf4
Alexander Borsuk <me@alex.bio>
2024-07-29 20:42:12 +0200

    Enable donates for GP android 240729
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- src/servers.ts
+++ src/servers.ts
@@ -125,7 +125,7 @@ export async function getServersList(request: Request) {
 
   // Disable donates for reviewers for all app versions AFTER this one.
   const lastApprovedAndReleasedGoogleAppVersionCode = 240723;
-  const lastApprovedAndReleasediOSAppVersionCode = 240727;
+  const lastApprovedAndReleasediOSAppVersionCode = 240729;
   let donatesEnabled = true;
   if (
     appVersion.flavor === 'google' &&
